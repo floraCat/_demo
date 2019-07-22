21d58a6351ac1242fb5fcab297f773027d3b5696
chenfanghua <1781721016@qq.com>
2019-07-22 20:33:33 +0800

    上传下载

---
+++ xx_框架/vue/vue-refer/src/components/grid-layout/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <grid-layout
+    :layout.sync="layout"
+    :col-num="12"
+    :row-height="30"
+    :is-draggable="flag"
+    :is-resizable="true"
+    :is-mirrored="false"
+    :vertical-compact="true"
+    :margin="[10, 10]"
+    :use-css-transforms="true"
+    @layout-created="layoutCreatedEvent"
+    @layout-before-mount="layoutBeforeMountEvent"
+    @layout-mounted="layoutMountedEvent"
+    @layout-ready="layoutReadyEvent"
+    @layout-updated="layoutUpdatedEvent"
+  >
+
+    <grid-item v-for="item in layout"
+               :x="item.x"
+               :y="item.y"
+               :w="item.w"
+               :h="item.h"
+               :i="item.i"
+               @resize="resizeEvent"
+               @move="moveEvent"
+               @resized="resizedEvent"
+               @moved="movedEvent"
+    >
+      <p style="width: 50px; height: 50px; background: blue;" @movestart="test">{{item.i}}</p>
+    </grid-item>
+  </grid-layout>
+</template>
+
+<script>
+
+  var testLayout = [
+    {"x":0,"y":0,"w":2,"h":2,"i":"0"},
+    {"x":2,"y":0,"w":2,"h":4,"i":"1"},
+    {"x":4,"y":0,"w":2,"h":5,"i":"2"},
+    {"x":6,"y":0,"w":2,"h":3,"i":"3"},
+    {"x":8,"y":0,"w":2,"h":3,"i":"4"},
+    {"x":10,"y":0,"w":2,"h":3,"i":"5"},
+    {"x":0,"y":5,"w":2,"h":5,"i":"6"},
+    {"x":2,"y":5,"w":2,"h":5,"i":"7"},
+    {"x":4,"y":5,"w":2,"h":5,"i":"8"},
+    {"x":6,"y":3,"w":2,"h":4,"i":"9"},
+    {"x":8,"y":4,"w":2,"h":4,"i":"10"},
+    {"x":10,"y":4,"w":2,"h":4,"i":"11"},
+    {"x":0,"y":10,"w":2,"h":5,"i":"12"},
+    {"x":2,"y":10,"w":2,"h":5,"i":"13"},
+    {"x":4,"y":8,"w":2,"h":4,"i":"14"},
+    {"x":6,"y":8,"w":2,"h":4,"i":"15"},
+    {"x":8,"y":10,"w":2,"h":5,"i":"16"},
+    {"x":10,"y":4,"w":2,"h":2,"i":"17"},
+    {"x":0,"y":9,"w":2,"h":3,"i":"18"},
+    {"x":2,"y":6,"w":2,"h":2,"i":"19"}
+  ];
+
+  import VueGridLayout from 'vue-grid-layout';
+export default {
+  name: 'table-pagination',
+  components: {
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem
+  },
+  data () {
+    return {
+      layout: testLayout,
+      flag: true
+    }
+  },
+  methods: {
+    test (ev) {
+      console.log(333333);
+      ev.preventDefault();
+      this.flag = false;
+    },
+    layoutCreatedEvent (newLayout) {
+      console.log('layoutCreatedEvent', newLayout);
+    },
+    layoutBeforeMountEvent (newLayout) {
+      console.log('layoutBeforeMountEvent', newLayout);
+    },
+    layoutMountedEvent (newLayout) {
+      console.log('layoutMountedEvent', newLayout);
+    },
+    layoutReadyEvent (newLayout) {
+      console.log('layoutReadyEvent', newLayout);
+    },
+    layoutUpdatedEvent (newLayout) {
+      console.log('layoutUpdatedEvent', newLayout);
+    },
+
+    resizeEvent (i, newH, newW, newHPx, newWPx) {
+      console.log('resizeEvent', i, newH, newW, newHPx, newWPx);
+    },
+    moveEvent (i, newX, newY) {
+      console.log('moveEvent', i, newX, newY);
+    },
+    resizedEvent (i, newH, newW, newHPx, newWPx) {
+      console.log('resizedEvent', i, newH, newW, newHPx, newWPx);
+    },
+    movedEvent (i, newX, newY) {
+      console.log('movedEvent', i, newX, newY);
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+  .vue-grid-item {
+    background: red;
+  }
+</style>
--- xx_框架/vue/vue-refer/src/router/components.js
+++ xx_框架/vue/vue-refer/src/router/components.js
@@ -1,9 +1,15 @@
 import tablePagination from '@/components/table-pagination'
+import gridLayout from '@/components/grid-layout'
 
 export default [
     {
       path: '/table-pagination',
       name: 'table-pagination',
       component: tablePagination
+    },
+  {
+      path: '/grid-layout',
+      name: 'grid-layout',
+      component: gridLayout
     },
 ];
